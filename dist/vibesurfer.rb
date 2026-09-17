c4f17beefb849a41094a7069f6b5276caf3b2ac5
Frane Bandov <frane@offbyte.com>
2026-09-17 16:54:23 +0200

    dist: v0.2.4 source-formula sha256

--- dist/vibesurfer.rb
+++ dist/vibesurfer.rb
@@ -14,9 +14,9 @@
 class Vibesurfer < Formula
   desc "A browser for LLMs, not humans."
   homepage "https://github.com/frane/vibesurfer"
-  url "https://github.com/frane/vibesurfer/archive/refs/tags/v0.2.3.tar.gz"
+  url "https://github.com/frane/vibesurfer/archive/refs/tags/v0.2.4.tar.gz"
   # Update on each release: `shasum -a 256 <tarball>`.
-  sha256 "20df08e56ff34606c120954122e75cafc352bc3540d060297e9fb8e19e3c255c"
+  sha256 "4794e7565f324110a56c411677b4ca7dc811b8e6c8581d3ada556acbbd015346"
   license "Apache-2.0"
   head "https://github.com/frane/vibesurfer.git", branch: "main"
 
